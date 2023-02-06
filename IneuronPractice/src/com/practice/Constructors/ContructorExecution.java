5702307ad2b83b7d6a51471a8a37703f75a4e0a8
MalepatiJayakumar <111275613+MalepatiJayakumar@users.noreply.github.com>
2023-02-06 07:54:19 +0530

    Constructor Execution

---
+++ IneuronPractice/src/com/practice/Constructors/ContructorExecution.java
@@ -0,0 +1,46 @@
+package com.practice.Constructors;
+
+class Parent{
+	int a, b;
+	Parent(){
+		a = 10;
+		b = 20;
+		System.out.println("Inside Parent Constructor");
+	}
+	Parent(int a, int b){
+		this.a = a;
+		this.b = b;
+		System.out.println("Inside Parent parametarized Constructor");
+	}
+}
+class Child extends Parent{
+	int x, y;
+	Child(){
+		//Super() --> if we are not using this() method then by default super() method will be called where parent constructor will get executed,
+                     // so a & b values will get initialized
+		x = 100;
+		y = 200;
+		System.out.println("Inside Child Constructor");
+	}
+	Child(int x, int y){
+		this.x = x;
+		this.y = y;
+		System.out.println("Inside Child parametarized Constructor");
+	}
+	void disp() {
+		System.out.println(a+" "+b+" "+x+" "+y);
+	}
+	
+}
+public class ContructorExecution {
+	public static void main(String[] args) {
+		Child ch = new Child();
+		ch.disp();
+	}
+}
+//Output :- 
+/*
+ * Inside Parent Constructor 
+ * Inside Child Constructor 
+ * 10 20 100 200
+ */
