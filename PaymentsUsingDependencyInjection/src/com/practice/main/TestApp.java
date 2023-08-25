9abf13c949f2c04fd994495abd69f7999bebcad2
MalepatiJayakumar <111275613+MalepatiJayakumar@users.noreply.github.com>
2023-08-25 09:17:04 +0530

    [25 Aug 2023] changes

--- PaymentsUsingDependencyInjection/src/com/practice/main/TestApp.java
+++ PaymentsUsingDependencyInjection/src/com/practice/main/TestApp.java
@@ -1,11 +1,16 @@
 package com.practice.main;
 
 import com.practice.serviceImpl.CreditCard;
+import com.practice.serviceImpl.DebitCard;
 
 public class TestApp {
 	public static void main(String[] args) {
 		/* Payment will be processed from credit card */
 		ProcessPayment pp = new ProcessPayment(new CreditCard());
+		
+		/* As we are overriding the executable class , here the bill Amount will be executed from Debit Card */
+		pp.setCard(new DebitCard());
+		
 		Boolean status = pp.processPayment(5000.00d);
 		if(status) {
 			System.out.println("Payment has been processed successfully , thank you for visiting..");
