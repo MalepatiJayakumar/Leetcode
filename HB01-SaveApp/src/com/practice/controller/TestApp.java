d04b3b805983967785c1465ac4e4d5eacb02734f
MalepatiJayakumar <111275613+MalepatiJayakumar@users.noreply.github.com>
2023-08-31 09:34:52 +0530

    [31 AUG 2023] Hibernate Project to save sample entity by creating table on fly

---
+++ HB01-SaveApp/src/com/practice/controller/TestApp.java
@@ -0,0 +1,45 @@
+package com.practice.controller;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.practice.entity.Student;
+
+public class TestApp {
+	
+	public static void main(String[] args) {
+		// 1. Inform JVM to activate HIBERNATE
+				Configuration configuration = new Configuration();
+				configuration.configure();
+
+				// Creating a SessionFactory object to hold many other objects required for
+				// HIBERNATE
+				SessionFactory sessionFactory = configuration.buildSessionFactory();
+
+				// Using SessionFactory object, get only one Session object to perform our
+				// persistence operation
+				Session session = sessionFactory.openSession();// connection object,ORM-dialects,L1-Cache,TXTManagement
+
+				Transaction transaction = session.beginTransaction();// transaction.begin()
+
+				// 2. Create PERSITENCE Object
+				Student student = new Student();
+				student.setId(10l);
+				student.setAge(49);
+				student.setName("sachin");
+
+				// 3. Perform PERISTENCE operation using Entity/Model/POJO object
+				session.save(student);
+
+				// 4. commit the operation based on the result
+				transaction.commit();
+
+				System.out.println("Object saved to database....");
+
+				session.close();
+				sessionFactory.close();
+
+	}
+}
---
+++ HB01-SaveApp/src/com/practice/entity/Student.java
@@ -0,0 +1,55 @@
+package com.practice.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="HBSTUDENT")
+public class Student {
+
+	@Id
+	@Column(name="id")
+	private Long id;
+	
+	@Column(name="name" , length=20)
+	private String name;
+	
+	@Column(name="age")
+	private Integer age;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+	public Student() {
+		System.out.println("Hibernate internally uses zero args constructor");
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", age=" + age + "]";
+	}
+}
