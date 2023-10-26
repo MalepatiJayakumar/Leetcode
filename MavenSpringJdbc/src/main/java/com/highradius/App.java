43c3a2eef5e8aa13007a729967ec498f7adb1316
MalepatiJayakumar <111275613+MalepatiJayakumar@users.noreply.github.com>
2023-10-26 08:30:14 +0530

    [26 OCT 2023] changes JDBCTemplate

--- MavenSpringJdbc/src/main/java/com/highradius/App.java
+++ MavenSpringJdbc/src/main/java/com/highradius/App.java
@@ -1,5 +1,8 @@
 package com.highradius;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
@@ -7,7 +10,13 @@ import org.springframework.stereotype.Component;
 public class App {
 
 	private static final String SELECT_COUNT_QUERY = "SELECT COUNT(*) FROM employee";
-
+	private static final String SELECT_NAME_BY_ID = "select `name` from employee where pk_id=?";
+	private static final String SELECT_EMPLOYEE_FIELDS_BY_ID = "SELECT `name`,`age`,`address`,`salary` FROM employee WHERE pk_id=?";
+	private static final String SELECT_EMPLOYEES_BY_LOCATION="SELECT `name`,`age`,`address`,`salary` FROM employee WHERE address IN (?,?)";
+	private static final String INSERT_EMPLOYEE = "INSERT INTO employee(`name`,`age`,`address`,`salary`,`is_deleted`) VALUES (?,?,?,?,0)";
+	private static final String UPDATE_EMPLOYEE_SALARY = "UPDATE employee SET salary=salary+? WHERE `address`=?";
+	
+	
 	static {
 		System.out.println("App.class file is getting loaded");
 	}
@@ -18,8 +27,29 @@ public class App {
 		this.jdbcTemplate = jdbcTemplate;
 	}
 
-	public int test() {
+	public int getEmployeeCount() {
 		System.out.println(jdbcTemplate);
 		return jdbcTemplate.queryForObject(SELECT_COUNT_QUERY,Integer.class);
 	}
+	
+	public String getEmployeeNameById(Integer eId) {
+		return jdbcTemplate.queryForObject(SELECT_NAME_BY_ID,String.class,eId);
+	}
+	
+	public Map<String,Object> getEmployeeById(Integer eId){
+		return jdbcTemplate.queryForMap(SELECT_EMPLOYEE_FIELDS_BY_ID,eId);
+	}
+	
+	public List<Map<String,Object>> getEmployeesByLocation(String loc1,String loc2){
+		return jdbcTemplate.queryForList(SELECT_EMPLOYEES_BY_LOCATION,loc1,loc2);
+	}
+
+	public Integer insertEmployee(String name, Integer age, String address, Double salary) {
+		return jdbcTemplate.update(INSERT_EMPLOYEE, name, age, address, salary);
+	}
+	
+	public Integer updateSalary(String location , Double bonus) {
+		return jdbcTemplate.update(UPDATE_EMPLOYEE_SALARY,bonus,location);
+	}
+	
 }
--- MavenSpringJdbc/src/main/java/com/highradius/TestApp.java
+++ MavenSpringJdbc/src/main/java/com/highradius/TestApp.java
@@ -10,7 +10,20 @@ public class TestApp {
 	public static void main(String[] args) {
 		ApplicationContext context = new AnnotationConfigApplicationContext(Configuration1.class);
 		App app = context.getBean(App.class);
-		System.out.println(app.test());
+		System.out.println("Total number if employees :: " + app.getEmployeeCount());
+		System.out.println("Employee name :: " + app.getEmployeeNameById(1));
+		System.out.println("Employee Details by Id :: " + app.getEmployeeById(1));
+		System.out.println("Employee Details where location in HYD & BLR :: " + app.getEmployeesByLocation("HYD", "BLR"));
+
+		/** Inserting an new employee **/
+//		System.out.println(app.insertEmployee("Saravana", 24, "STVD", 61000.0d) > 0 ? "Record has been inserted successfully"
+//				: "Inserting an Record has been failed");
+		
+		/** Updating employee salary based on location **/
+		System.out.println("Number of employees salary modified :: "+app.updateSalary("HYD",1000.0));
+		
+		
+		
 		((AbstractApplicationContext) context).close();
 	}
 }
