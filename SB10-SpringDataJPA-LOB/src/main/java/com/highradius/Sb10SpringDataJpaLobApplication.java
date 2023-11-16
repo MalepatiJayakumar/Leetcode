6173e5fcf86c6b9f3e5647d000771654c5464b73
MalepatiJayakumar <111275613+MalepatiJayakumar@users.noreply.github.com>
2023-11-16 09:33:07 +0530

    [16 NOV 2023] changes for spring data jpa

---
+++ SB10-SpringDataJPA-LOB/src/main/java/com/highradius/Sb10SpringDataJpaLobApplication.java
@@ -0,0 +1,13 @@
+package com.highradius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb10SpringDataJpaLobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb10SpringDataJpaLobApplication.class, args);
+	}
+
+}
