3c220d8aa97961a522e6fab58c8f17b200398d47
MalepatiJayakumar <111275613+MalepatiJayakumar@users.noreply.github.com>
2023-11-21 09:33:04 +0530

    [21 NOV 2023]

---
+++ SB11-Spring-Data-MongoDB/src/main/java/com/highradius/Sb11SpringDataMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.highradius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb11SpringDataMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb11SpringDataMongoDbApplication.class, args);
+	}
+
+}
